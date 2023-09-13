c82693862d3423a9abe7840980ddcc93c121efce
Tardanes <tardanes@vivaldi.net>
2023-09-13 14:34:07 +0300

    Test commit

--- src/HelloWorldDemo.java
+++ src/HelloWorldDemo.java
@@ -1,9 +1,12 @@
+import java.time.LocalDate; // ця бібліотека дуже корисна!
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
-public class Main {
-    public static void main(String[] args) {
-        // Press Alt+Enter with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
-        
+public class HelloWorldDemo {
+    public static void main(String[] argv) {
+        System.out.println("Hello world;");
+        System.out.println("This task done by Khamov Ivan [PZ-20-1/9].");
+        System.out.println(LocalDate.now()); // 1
+        // new comment
+        // yeeeeeeeeeee
     }
 }
